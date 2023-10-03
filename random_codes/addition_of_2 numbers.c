5e39697972d21e4be1ac52c99772ab538951e673
amritanshu ranjan <amritanshuranjan04@gmail.com>
2023-10-03 21:49:55 +0530

    changes made and new file added

---
+++ random_codes/addition_of_2 numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=0,c=0;
+
+    c= a+b;
+
+    printf("enter the value of a & b\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    c= a+b; 
+    
+    printf("addition is %d",c);
+
+}
