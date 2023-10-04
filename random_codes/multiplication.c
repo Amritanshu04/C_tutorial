1fd195b8cb83d308edcf6f074b8254cc555d4ec8
amritanshu ranjan <amritanshuranjan04@gmail.com>
2023-10-04 23:10:49 +0530

    new file added

---
+++ random_codes/multiplication.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=0,c=0;
+
+    printf("enter the value of a & b\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    c= a*b; 
+    
+    printf("Multiplication is %d",c);
+
+}
