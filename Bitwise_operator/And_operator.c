72cc86f8c8e04a9055333a46baa50e84f454577a
amritanshu ranjan <amritanshuranjan04@gmail.com>
2023-10-10 00:16:28 +0530

    added file of Bitwise operator

---
+++ Bitwise_operator/And_operator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=6;
+   int b=4;  // variable declarations  
+   printf("The output of the Bitwise AND operator a&b is %d",a&b);  
+   return 0;  
+}  
