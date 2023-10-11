db6e59b948a36d926a9b44bcda56cb91174253d0
amritanshu ranjan <amritanshuranjan04@gmail.com>
2023-10-11 14:53:52 +0530

    added file of Bitwise operator

--- Bitwise_operator/Xor_operator.c
+++ Bitwise_operator/Xor_operator.c
@@ -3,6 +3,6 @@ int main()
 {  
    int a=6;
    int b=4;  // variable declarations  
-   printf("The output of the Bitwise OR operator a|b is %d",a|b);  
+   printf("The output of the Bitwise XOR operator a^b is %d",a^b);  
    return 0;  
 }  
