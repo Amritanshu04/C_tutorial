dcc4d53615f8c2c1f77e00d8bb3e3aa54986678e
amritanshu ranjan <amritanshuranjan04@gmail.com>
2023-10-14 23:17:18 +0530

    added file of String

---
+++ String/string(1).c
@@ -0,0 +1,13 @@
+// C program to print Integer data types.
+#include <stdio.h>
+ 
+int main()
+{
+    char a [10];
+    printf("enter the username : ");
+    scanf("%s",&a);
+
+    printf("%s",a);
+ 
+    return 0;
+}
